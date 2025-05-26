fa90374e757add6f21ba7a2d4abbf945c1db661d
Beybut Abdulrahimov <beybut2013@gmail.com>
2025-05-26 19:26:30 +0400

    hits rate

--- main_test.go
+++ main_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"BeyCoder/keydb-vs-ristretto/config"
-	"context"
 	"fmt"
 	"github.com/dgraph-io/ristretto/v2"
 	"github.com/redis/go-redis/v9"
@@ -11,16 +10,13 @@ import (
 )
 
 const KeyBase = "key3"
-const CacheIterations = 100_000
+const CacheIterations = 1_000_000
 
 func init() {
 	config.LoadConfig()
 }
 
 func TestRedisCache(t *testing.T) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
 	rdb := redis.NewClient(&redis.Options{
 		Network:  "unix",
 		Addr:     config.Values.KeyDB.Address,
@@ -34,19 +30,23 @@ func TestRedisCache(t *testing.T) {
 	for i := 0; i < CacheIterations; i++ {
 		key := fmt.Sprintf("%s%d", KeyBase, i)
 		value := fmt.Sprintf("%d", i)
-		if err := rdb.Set(ctx, key, value, 0).Err(); err != nil {
-			t.Fatalf("Redis Set error at i=%d: %v", i, err)
-		}
-	}
-	for i := 0; i < CacheIterations; i++ {
-		key := fmt.Sprintf("%s%d", KeyBase, i)
-		_, err := rdb.Get(ctx, key).Result()
-		if err != nil && err != redis.Nil {
-			t.Fatalf("Redis Get error at i=%d: %v", i, err)
+		if err := rdb.Set(t.Context(), key, value, 0).Err(); err != nil {
+			t.Fatalf("KeyDB Set error at i=%d: %v", i, err)
 		}
 	}
 
-	t.Logf("Redis test finished in %s", time.Since(start))
+	hits := 0
+	for i := 0; i < CacheIterations; i++ {
+		key := fmt.Sprintf("%s%d", KeyBase, i)
+		_, err := rdb.Get(t.Context(), key).Result()
+		if err != nil && err != redis.Nil {
+			continue
+		}
+		hits++
+	}
+
+	t.Logf("KeyDB test finished in %s", time.Since(start))
+	t.Logf("KeyDB hits: %d / %d (%.2f%%)", hits, CacheIterations, float64(hits)*100/CacheIterations)
 }
 
 func TestRistrettoCache(t *testing.T) {
